d08ae9a5ab7ebce1288cb17cdb2a93b1ce862986
Omar 🍉 <o_yousaf@live.co.uk>
2025-12-11 19:54:59 +0000

    squashing bugs

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ const nextConfig: NextConfig = {
   // Disable source-maps in dev + prod (stops invalid-source-map warnings)
   productionBrowserSourceMaps: false,
 
-  webpack(config) {
-    // Disable sourcemaps entirely at build + dev runtime
-    config.devtool = false;
-    return config;
-  },
 };
 
 export default nextConfig;
