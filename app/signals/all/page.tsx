6546d3dbb94ce213373ef4dd9bce639d9c7d62ca
Omar 🍉 <o_yousaf@live.co.uk>
2025-12-13 12:42:38 +0000

    stripping out signals

--- app/signals/all/page.tsx
+++
@@ -1,7 +0,0 @@
-import SignalClient from "@/components/signals/SignalClient";
-import { getSignalsAll } from "../actions/getSignals";
-
-export default async function AllSignalsPage() {
-  const signals = await getSignalsAll();
-  return <SignalClient initialSignals={signals} />;
-}
--- app/signals/page.tsx
+++
@@ -1,7 +0,0 @@
-import SignalClient from "@/components/signals/SignalClient";
-import { getSignalsAll } from "./actions/getSignals";
-
-export default async function SignalsPage() {
-  const signals = await getSignalsAll();
-  return <SignalClient initialSignals={signals} />;
-}
