610212dd5a3f8923a92f4c35cb320b622eeed013
Omar 🍉 <o_yousaf@live.co.uk>
2026-04-29 13:56:13 +0100

    ecosystem config added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,5 +10,15 @@ module.exports = {
         NODE_ENV: "production",
       },
     },
+
+    {
+      name: "omega-boot",
+      cwd: "/apps/0mega",
+      script: "bash",
+      args: "-c 'sleep 5 && curl -s http://localhost:3000/api/engine/boot'",
+
+      autorestart: false,
+      watch: false,
+    },
   ],
 };
